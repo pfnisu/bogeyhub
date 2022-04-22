6a6f44f4363408c7747dcaf53ced61c0eaf05972
Niko Suoniemi <niko.suoniemi@tuni.fi>
2022-04-22 09:50:17 +0300

    frontend/src/CreateComp.js: Add competition creation -component

---
+++ frontend/src/CreateComp.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {request} from './index';
+
+const path = 'competition';
+
+export const CreateComp = () => {
+    const dateRef = React.createRef();
+    const nameRef = React.createRef();
+
+    // Add new competition with only mandatory data
+    const addComp = async () => {
+        let newcomp = {
+            date: dateRef.current.value,
+            name: nameRef.current.value,
+            phase_id: 1,
+        };
+        // POST data to backend
+        await request(path, newcomp);
+        dateRef.current.value = "";
+        nameRef.current.value = "";
+    }
+    // Format current date for default form value
+    let currDate = new Date().toJSON().split('T')[0];
+
+    return (
+        <>
+            <h1>Admin</h1>
+            <form onSubmit={e => e.preventDefault()}>
+                <input ref={nameRef} type="text" placeholder="Competition name" />
+                <input ref={dateRef} type="date" defaultValue={currDate} />
+                <button onClick={() => addComp()}>Create competition</button>
+            </form>
+        </>
+
+    );
+};
