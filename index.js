3de4f0acc907753bfb58ddceeacf9a37e2b64953
Niko Suoniemi <niko.suoniemi@tuni.fi>
2022-04-22 10:48:53 +0300

    index.js: Initial backend

---
+++ index.js
@@ -0,0 +1,30 @@
+const competition = require('./competition.js');
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// Main function
+(async () => {
+    try {
+        app.use(express.json());
+        app.use((req, res, next) => {
+            res.header('Access-Control-Allow-Origin', '*');
+            next();
+        });
+        app.use(express.static('frontend/build'));
+        app.use('/competition', competition);
+
+        const server = app.listen(port, () => {
+            console.log('Listening on port ' + port);
+        });
+        process.on('SIGINT', async () => {
+            server.close(() => {
+                console.log('Server closed.');
+                process.exit(1);
+            });
+        });
+    } catch (err) {
+        console.log(err);
+    }
+})();
