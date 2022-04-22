d2d7fea9009dde19ec35b9fb48b161621625038e
Niko Suoniemi <niko.suoniemi@tuni.fi>
2022-04-22 09:53:11 +0300

    frontend/src/index.js: Import components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Routes, Route, NavLink} from 'react-router-dom';
+import {CreateComp} from './CreateComp';
+import {CompList} from './CompList';
 import './index.css';
+
+const uri = 'http://localhost:8080/';
+export const request = async (resource, data, method = 'POST') => {
+    try {
+        let resp = await fetch(uri + resource, {
+            method: method,
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify(data)
+        });
+        console.log(resp);
+        return resp;
+    } catch(e) {
+        console.log('DB update failed.');
+    }
+}
 
 const App = () => {
 
