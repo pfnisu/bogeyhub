7f063e9554ded556776ee5a79ab206a77304396d
Niko Suoniemi <niko.suoniemi@tuni.fi>
2022-04-25 09:58:26 +0300

    frontend/src/Register.js: Link to competition

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Link} from 'react-router-dom';
 
 export const Register = (props) => {
+    // UI mode: initial, register or info
     const [ui, setUi] = React.useState('initial');
 
+    // Make comp title into a link to comp view
+    let title = <Link to={'/competition/' + props.comp.id}>{props.comp.name}</Link>;
     return (
         <li className={props.comp.phase}>
             {ui === 'initial' && <>
                 <button onClick={() => setUi('register')}>Register</button>
                 <button onClick={() => setUi('info')}>View info</button>
-                <h2>{props.comp.name}</h2>
+                <h2>{title}</h2>
             </>}
             {ui === 'register' && <>
                 <button onClick={() => setUi('initial')}>Cancel</button>
                 <button onClick={() => setUi('info')}>View info</button>
-                <h2>{props.comp.name}</h2>
+                <h2>{title}</h2>
             </>}
             {ui === 'info' && <>
                 <button onClick={() => setUi('register')}>Register</button>
                 <button onClick={() => setUi('initial')}>Hide info</button>
-                <h2>{props.comp.name}</h2>
+                <h2>{title}</h2>
                 <span>{props.comp.info}</span>
             </>}
             <span>
