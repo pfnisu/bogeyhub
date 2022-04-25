1e9fed20794d7ef507a181b9726529a604b49585
Niko Suoniemi <niko.suoniemi@tuni.fi>
2022-04-25 09:55:37 +0300

    frontend/src/ScoreInput.js: Add score input

---
+++ frontend/src/ScoreInput.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {useParams} from 'react-router-dom';
+import {request} from './index';
+
+export const ScoreInput = (props) => {
+    const [competition, setCompetition] = React.useState({});
+    let params = useParams();
+
+    // GET competition with compId from backend at component mount
+    React.useEffect(() => {
+        (async () => {
+            let resp = await fetch(props.path + params.compId);
+            let json = await resp.json();
+            setCompetition(json);
+        })();
+    }, []);
+    return (
+        <>
+            <h1>{competition.name}</h1>
+        </>
+    );
+};
