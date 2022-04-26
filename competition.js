baefcfff3af135d4da5b00a823cb5989c27f7037
Niko Suoniemi <niko.suoniemi@tuni.fi>
2022-04-26 14:10:41 +0300

    competition.js: Handle requests for groups and results

--- competition.js
+++ competition.js
@@ -11,6 +11,7 @@ const compSchema = {
         name: { type: 'string', minLength: 1, maxLength: 100 },
         venue: { type: 'string', minLength: 1, maxLength: 100 },
         max_users: { type: 'integer', minimum: 1 },
+        info: { type: 'string'},
         phase_id: { type: 'integer', minimum: 1, maximum: 5 },
     },
     required: ['start_date', 'name', 'phase_id'],
@@ -32,6 +33,43 @@ competition.get('/', async (req, res) => {
 competition.get('/:id([0-9]+)', async (req, res) => {
     try {
         let result = await db.findById(req.params.id);
+        if (result) res.status(200).send(result);
+        else res.status(404).send('Id not found');
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+// Get results by competition id
+competition.get('/result/:id([0-9]+)', async (req, res) => {
+    try {
+        let result = await db.resultsById(req.params.id);
+        if (result) {
+            // Transpose column-oriented scores to per-player data
+            let set = new Set(result.map(obj => obj.user));
+            let arr = [];
+            set.forEach(usr => {
+                let score = [];
+                // TODO Null-pad
+                result.forEach(el => el.user === usr && score.push(el.result)),
+                arr.push({
+                    user: usr,
+                    scores: score,
+                    //scores: result.map(el => el.user === usr ? el.result : null),
+                });
+            });
+            res.status(200).send(arr);
+        }
+        else res.status(404).send('Id not found');
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+// Get groups for competition id
+competition.get('/group/:id([0-9]+)', async (req, res) => {
+    try {
+        let result = await db.groupsById(req.params.id);
         if (result) res.status(200).send(result);
         else res.status(404).send('Id not found');
     } catch (err) {
