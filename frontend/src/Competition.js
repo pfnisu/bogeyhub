63e1216e55b6e84919f820eb80e30cec604cbc4b
Niko Suoniemi <niko.suoniemi@tuni.fi>
2022-04-28 12:58:50 +0300

    frontend/src/Competition.js: Use unicode-symbols in ui

--- frontend/src/Competition.js
+++ frontend/src/Competition.js
@@ -24,45 +24,45 @@ export const Competition = (props) => {
         <>
             <h1>{competition.name}</h1>
             {ui === 'results' && <>
-                <button className='sel' onClick={() => setUi('results')}>Results</button>
-                <button onClick={() => setUi('info')}>View info</button>
-                <button onClick={() => setUi('groups')}>View groups</button>
+                <button className='sel' onClick={() => setUi('results')}>&#9733; Results</button>
+                <button onClick={() => setUi('info')}>&#8505; View info</button>
+                <button onClick={() => setUi('groups')}>&#119558; View groups</button>
                 {props.user !== '' &&
                     <button className='right' onClick={() => setUi('input')}>
-                        Input scores
+                        &#9998; Input scores
                     </button>
                 }
                 <ScoreTable path={props.path} id={params.compId} />
             </>}
             {ui === 'info' && <>
-                <button onClick={() => setUi('results')}>Results</button>
-                <button className='sel' onClick={() => setUi('info')}>View info</button>
-                <button onClick={() => setUi('groups')}>View groups</button>
+                <button onClick={() => setUi('results')}>&#9733; Results</button>
+                <button className='sel' onClick={() => setUi('info')}>&#8505; View info</button>
+                <button onClick={() => setUi('groups')}>&#119558; View groups</button>
                 {props.user !== '' &&
                     <button className='right' onClick={() => setUi('input')}>
-                        Input scores
+                        &#9998; Input scores
                     </button>
                 }
                 <p>{competition.info}</p>
             </>}
             {ui === 'input' && <>
-                <button onClick={() => setUi('results')}>Results</button>
-                <button onClick={() => setUi('info')}>View info</button>
-                <button onClick={() => setUi('groups')}>View groups</button>
+                <button onClick={() => setUi('results')}>&#9733; Results</button>
+                <button onClick={() => setUi('info')}>&#8505; View info</button>
+                <button onClick={() => setUi('groups')}>&#119558; View groups</button>
                 {props.user !== '' &&
                     <button className='sel right' onClick={() => setUi('input')}>
-                        Input scores
+                        &#9998; Input scores
                     </button>
                 }
                 <ScoreInput path={props.path} id={params.compId} />
             </>}
             {ui === 'groups' && <>
-                <button onClick={() => setUi('results')}>Results</button>
-                <button onClick={() => setUi('info')}>View info</button>
-                <button className='sel' onClick={() => setUi('groups')}>View groups</button>
+                <button onClick={() => setUi('results')}>&#9733; Results</button>
+                <button onClick={() => setUi('info')}>&#8505; View info</button>
+                <button className='sel' onClick={() => setUi('groups')}>&#119558; View groups</button>
                 {props.user !== '' &&
                     <button className='right' onClick={() => setUi('input')}>
-                        Input scores
+                        &#9998; Input scores
                     </button>
                 }
                 <Groups path={props.path} id={params.compId} />
