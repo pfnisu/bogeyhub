02e639ff5741bb820b631bd3129eca123047504c
Niko Suoniemi <niko.suoniemi@tuni.fi>
2022-04-28 19:32:38 +0300

    admin.js: Add patch method

--- admin.js
+++ admin.js
@@ -24,11 +24,24 @@ admin.post('/', async (req, res) => {
 // Delete competition matching url param
 admin.delete('/:id([0-9]+)', async (req, res) => {
     try {
-        let success = await db.deleteById(req.params.id);
+        let success = await db.deleteComp(req.params.id);
         if (success) res.status(204).end(); // Delete ok, 204 No Content
         else res.status(404).send('Id not found'); // Nothing deleted
     } catch (err) {
         res.status(500).send(err);
+    }
+});
+
+// Partial update with PATCH
+admin.patch('/:id([0-9]+)', async (req, res) => {
+    // Return 400 Bad Request if patched obj doesn't validate
+    if (validate(req.body, compSchema).errors.length > 0) {
+        res.status(400).send('Invalid competition data');
+    } else {
+        // Try to update db row that has id
+        let success = await db.updateComp(req.params.id, req.body);
+        if (success) res.status(204).end(); // Update ok, 204 No Content
+        else res.status(404).send('Id not found');
     }
 });
 
