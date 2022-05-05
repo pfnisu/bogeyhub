83440b5a3a91ea18269fb4ca59f8e061611e8ecd
Niko Suoniemi <niko.suoniemi@tuni.fi>
2022-05-05 11:09:14 +0300

    schema.js: Add user and reg schemas

--- schema.js
+++ schema.js
@@ -14,4 +14,29 @@ const competition = {
     maxProperties: 4,
 };
 
-module.exports = { competition };
+// Define valid spec for user object
+const user = {
+    type: 'object',
+    properties: {
+        name: { type: 'string' },
+        password: { type: 'string' },
+        role_id: { type: 'integer', minimum: 1 },
+        id: { type: 'integer', minimum: 1 },
+        sex: { type: 'string', pattern: /male|female/ },
+        age: { type: 'integer', minimum: 0, maximum: 150 },
+    },
+    maxProperties: 4,
+};
+
+// Define valid spec for registration object
+const registration = {
+    type: 'object',
+    properties: {
+        user_id: { type: 'integer', minimum: 1 },
+        competition_id: { type: 'integer', minimum: 1 },
+        division: { type: 'string', pattern: /MPO|FPO/ },
+    },
+    required: ['user_id', 'competition_id', 'division'],
+    maxProperties: 3,
+};
+module.exports = { competition, user, registration };
