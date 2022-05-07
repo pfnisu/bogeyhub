b016b08fb983ae8e197cfa3e40d0cd9011b81b04
Niko Suoniemi <niko.suoniemi@tuni.fi>
2022-05-07 15:09:19 +0300

    frontend/src/ScoreInput.js: UI mockup

--- frontend/src/ScoreInput.js
+++ frontend/src/ScoreInput.js
@@ -6,6 +6,10 @@ import {request, path} from './util';
 export const ScoreInput = (props) => {
     const [competition, setCompetition] = React.useState({});
     const params = useParams();
+    const p1Ref = React.createRef();
+    const p2Ref = React.createRef();
+    const p3Ref = React.createRef();
+    const p4Ref = React.createRef();
 
     // GET competition with compId from backend at component mount
     React.useEffect(() => {
@@ -18,6 +22,26 @@ export const ScoreInput = (props) => {
     return (
         <>
             <h2>Score input</h2>
+            <form onSubmit={e => e.preventDefault()}>
+                <span className="score">
+                    <label>{props.user.name}</label>
+                    <input ref={p1Ref} type='number' autoFocus />
+                </span>
+                <span className="score">
+                    <label>{props.user.name}</label>
+                    <input ref={p2Ref} type='number' />
+                </span>
+                <span className="score">
+                    <label>{props.user.name}</label>
+                    <input ref={p3Ref} type='number' />
+                </span>
+                <span className="score">
+                    <label>{props.user.name}</label>
+                    <input ref={p4Ref} type='number' />
+                </span>
+                <button onClick={(ev) => prev(ev)}>&#171; Prev hole</button>
+                <button onClick={(ev) => next(ev)}>Next hole &#187;</button>
+            </form>
         </>
     );
 };
