593723ff9788d206aaedd69b0f4cc0d71a31db77
Niko Suoniemi <niko.suoniemi@tuni.fi>
2022-05-11 14:04:44 +0300

    frontend/src/Groups.js: Use data via props

--- frontend/src/Groups.js
+++ frontend/src/Groups.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {useParams} from 'react-router-dom';
 import {request, path} from './util';
 
 export const Groups = (props) => {
-    const [groups, setGroups] = React.useState([]);
-    const params = useParams();
-
-    // GET groups for compId
-    React.useEffect(() => {
-        (async () => {
-            props.setErr(null);
-            let resp = await request(path.comp + 'group/' + params.compId);
-            resp ? setGroups(resp) : props.setErr('Loading groups failed');
-        })();
-    }, []);
-
-    let table = groups.map((grp, idx) => {
+    let table = props.groups.map((grp, idx) => {
         return (
             <tr key={idx} className='score'>
                 <td>{grp.start_position}</td><td>{grp.user}</td>
