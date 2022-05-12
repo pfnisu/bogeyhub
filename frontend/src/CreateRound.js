b003ce546e090ced4d1355efc18b168428846d05
Niko Suoniemi <niko.suoniemi@tuni.fi>
2022-05-12 14:03:15 +0300

    frontend/src/CreateRound.js: Separate round and group forms, check inputs, disable after send

--- frontend/src/CreateRound.js
+++ frontend/src/CreateRound.js
@@ -1,36 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {request, path} from './util';
+import {request, path, check} from './util';
 
 export const CreateRound = (props) => {
     const [courses, setCourses] = React.useState([]);
     const [course, setCourse] = React.useState({});
+    const [round, setRound] = React.useState({});
     const [holes, setHoles] = React.useState([]);
     const [groups, setGroups] = React.useState([]);
     const [idGroups, setIdGroups] = React.useState([]);
     const timeRef = React.createRef();
+    const courseRef = React.createRef();
 
     // Add new competition with only mandatory data
     const addRound = async (ev) => {
         props.setErr(null);
+        if (!check(timeRef)) return false;
+        if (!check(courseRef)) return false;
         let data = {
             start_time: timeRef.current.value + ':00',
             course_id: course.id,
         };
 
         // POST round data first to get round_id
-        let round = await request(path.admin + 'round/' + props.competition.id, 'POST', data);
-        if (!round) {
-            props.setErr('Round creation failed');
-            return false;
-        }
-        // POST group data
-        console.log(groups);
-        let resp = await request(path.admin + 'group/' + round.id, 'POST', idGroups);
+        let resp = await request(path.admin + 'round/' + props.competition.id, 'POST', data);
         if (resp) {
             ev.target.classList.add('ok');
             ev.target.innerHTML = '&#10003; Created';
-        } else props.setErr('Group creation failed');
+            ev.target.disabled = true;
+            setRound(resp);
+        } else props.setErr('Round creation failed');
     }
 
     // Generate groups for the round
@@ -46,14 +45,29 @@ export const CreateRound = (props) => {
             let names = resp.map((reg) => reg.user);
             let idgrps = [];
             let namegrps = [];
-            console.log(names);
             for (let i = 0; i < ids.length; i += 4) {
                 idgrps.push(ids.slice(i, i + 4));
                 namegrps.push(names.slice(i, i + 4));
             }
             setGroups(namegrps);
             setIdGroups(idgrps);
+
+            console.log(idgrps);
         } else props.setErr('Failed to create groups');
+    }
+
+    // POST group data separately to allow editing
+    const addGroups = async (ev) => {
+        props.setErr(null);
+        if (!round.id) {
+            props.setErr('Round id not found. Create round first');
+            return false;
+        }
+        let resp = await request(path.admin + 'group/' + round.id, 'POST', idGroups);
+        if (resp) {
+            ev.target.classList.add('ok');
+            ev.target.innerHTML = '&#10003; Saved';
+        } else props.setErr('Group creation failed');
     }
 
     // Get list of courses
@@ -78,21 +92,25 @@ export const CreateRound = (props) => {
     return (
         <>
             <h1>Create round and groups: {props.competition.name}</h1>
-            <button onClick={() => props.setUi('edit')}>&#171; Cancel</button>
+            <button onClick={() => props.setUi('edit')}>&#171; Back</button>
             <form onSubmit={e => e.preventDefault()}>
-                <label>
-                    <input type="checkbox" defaultChecked={false} onChange={(ev) => setGolfStart(ev.target.value)} />
-                    Golf start (leave unchecked for shotgun start)
-                    &nbsp;Start time:
-                </label>
+                <label>Start time:</label>
                 <input ref={timeRef} type='time' autoFocus />
                 <label>Course:</label>
-                <input type='text' list='courses' onChange={(ev) =>
+                <input ref={courseRef} type='text' list='courses' onChange={(ev) =>
                     setCourse(state => courses.find((c) => c.name === ev.target.value) || state)} />
                 <datalist id='courses'>
                     {courses.map((c, idx) => <option value={c.name}>{c.name}</option>)}
                 </datalist>
-                {course.id && <p>&#9873; {course.name}, par {holes.map((h) => h.par).reduce((p, sum) => sum += p, 0)}</p>}
+                {course.id && <p>&#9873; {course.name}, par {holes.map((h) => h.par).reduce((sum, p) => sum += p, 0)}</p>}
+                <button onClick={(ev) => addRound(ev)}>&#10023; Create round</button>
+            </form>
+            <h2>Round id: {round.id || 'not created'}</h2>
+            <form onSubmit={e => e.preventDefault()}>
+                <label>
+                    <input type="checkbox" defaultChecked={false} onChange={(ev) => setGolfStart(ev.target.value)} />
+                    Golf start (leave unchecked for shotgun start)
+                </label>
                 <table>
                     <tbody>
                         <tr className='score'>
@@ -107,7 +125,7 @@ export const CreateRound = (props) => {
                     </tbody>
                 </table>
                 <button onClick={() => createGroups()}>&#9776; Generate groups</button>
-                <button onClick={(ev) => addRound(ev)}>&#10023; Create</button>
+                <button onClick={(ev) => addGroups(ev)}>Save groups</button>
             </form>
         </>
     );
