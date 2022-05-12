efe937d9291bdb4d7aac6751eb1b58c1599c9cb7
Niko Suoniemi <niko.suoniemi@tuni.fi>
2022-05-12 14:04:14 +0300

    frontend/src/ScoreInput.js: Implement proper score input

--- frontend/src/ScoreInput.js
+++ frontend/src/ScoreInput.js
@@ -1,44 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {useParams} from 'react-router-dom';
-import {request, path} from './util';
+import {request, path, check} from './util';
 
 export const ScoreInput = (props) => {
-    const [competition, setCompetition] = React.useState({});
     const params = useParams();
-    const p1Ref = React.createRef();
-    const p2Ref = React.createRef();
-    const p3Ref = React.createRef();
-    const p4Ref = React.createRef();
 
-    // GET competition with compId from backend at component mount
-    React.useEffect(() => {
-        (async () => {
-            props.setErr(null);
-            let resp = await request(path.comp + params.compId);
-            resp ? setCompetition(resp) : props.setErr('Loading competition failed');
-        })();
-    }, []);
+    // POST scores and advance to next hole
+    const next = async (ev) => {
+        props.setErr(null);
+        let inputs = document.querySelectorAll('input');
+        let data = [];
+        // Push all groups scores to data
+        for (let i = 0; i<inputs.length; i++) {
+            // Hack to work around refs
+            if (!check({current: inputs[i]})) return false;
+            data.push({
+                result: inputs[i].value,
+                hole_id: props.hole.id,
+                user_id: props.group[i].id,
+            });
+        }
+
+        // POST data to backend
+        let resp = await request(path.user + 'score/' + props.round.id, 'POST', data);
+
+        if (resp) {
+            props.setHole(state => {
+                let idx = ++state.index;
+                // Wrap to first hole after last
+                if (idx > props.round.holes.length) idx = 0;
+                // Clear inputs
+                inputs.forEach(i => i.value = '');
+                return {
+                    index: idx,
+                    id: props.round.holes[idx].id,
+                    name: props.round.holes[idx].name,
+                    par: props.round.holes[idx].par,
+                };
+            });
+        } else props.setErr('Failed to save scores');
+    };
+
+    // Return to previous hole
+    const prev = (ev) => {
+    };
+
+    let form = props.group.map((user, idx) =>
+        <span className="score" key={idx}>
+            <label>{user.name}</label>
+            <input type='number' />
+        </span>
+    );
+
     return (
         <>
-            <h2>Score input</h2>
+            <p>&#9873; {props.round.course}: hole {props.hole.name}, par {props.hole.par}</p>
             <form onSubmit={e => e.preventDefault()}>
-                <span className="score">
-                    <label>{props.user.name}</label>
-                    <input ref={p1Ref} type='number' autoFocus />
-                </span>
-                <span className="score">
-                    <label>{props.user.name}</label>
-                    <input ref={p2Ref} type='number' />
-                </span>
-                <span className="score">
-                    <label>{props.user.name}</label>
-                    <input ref={p3Ref} type='number' />
-                </span>
-                <span className="score">
-                    <label>{props.user.name}</label>
-                    <input ref={p4Ref} type='number' />
-                </span>
+                {form}
                 <button onClick={(ev) => prev(ev)}>&#171; Prev hole</button>
                 <button onClick={(ev) => next(ev)}>Next hole &#187;</button>
             </form>
