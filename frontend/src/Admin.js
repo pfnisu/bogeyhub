43e47fe3826b24ae1f9f5cc0d0082eedb6479d3e
Niko Suoniemi <niko.suoniemi@tuni.fi>
2022-05-17 11:06:24 +0300

    frontend/src/Admin.js: Round listing and deletion

--- frontend/src/Admin.js
+++ frontend/src/Admin.js
@@ -10,34 +10,46 @@ export const Admin = (props) => {
     // UI mode: create, edit, delete
     const [ui, setUi] = React.useState('create');
     const [competition, setCompetition] = React.useState({});
+    const [rounds, setRounds] = React.useState([]);
     const params = useParams();
 
     const dateRef = React.createRef();
-    const enddateRef = React.createRef();
     const nameRef = React.createRef();
+    const venueRef = React.createRef();
     const infoRef = React.createRef();
 
-    const getComp = async () => {
-        props.setErr(null);
-        if (params.compId == undefined) return false;
-        let resp = await request(path.comp + params.compId);
-
-        // Format start_date for default form value
-        resp.start_date = resp.start_date.split('T')[0];
-        resp ? setCompetition(resp) : props.setErr('Loading competition failed');
-        setUi('edit');
-    }
-
     // GET competition with compId at component mount
-    React.useEffect(() => { getComp() }, []);
+    React.useEffect(() => {
+        (async () => {
+            props.setErr(null);
+            if (params.compId == undefined) return false;
+            let resp = await request(path.comp + params.compId);
+
+            // Format start_date for default form value
+            resp.start_date = resp.start_date.split('T')[0];
+            resp ? setCompetition(resp) : props.setErr('Loading competition failed');
+            setUi('edit');
+        })();
+    }, []);
+
+    // GET rounds belonging to the comp
+    React.useEffect(() => {
+        competition.id && (async () => {
+            props.setErr(null);
+            let resp = await request(path.comp + 'rounds/' + competition.id);
+            resp ? setRounds(resp) : props.setErr('Loading rounds failed');
+        })();
+    }, [competition, ui]);
 
     // PATCH competition with edited data
     const editComp = async (ev) => {
+        props.setErr(null);
         ev.target.classList.remove('ok');
         if (!check(nameRef)) return false;
         let data = {
             start_date: dateRef.current.value,
             name: nameRef.current.value,
+            venue: venueRef.current.value,
             info: infoRef.current.value,
             phase_id: 1,
         };
@@ -55,30 +67,56 @@ export const Admin = (props) => {
         props.setErr(null);
         if (params.compId == undefined) return false;
         let resp = await request(path.admin + params.compId, 'DELETE');
-
         resp ? setUi('create') : props.setErr('Deletion failed');
     }
 
-    // TODO show save success/fail in ui (spinner/ checkmark in button)
+    // DELETE round
+    const delRound = async (ev) => {
+        props.setErr(null);
+        ev.target.classList.remove('ok');
+        if (ev.target.id == undefined) return false;
+        let resp = await request(path.admin + 'round/' + ev.target.id, 'DELETE');
+        if (resp) {
+            ev.target.classList.add('ok');
+            ev.target.innerHTML = '&#10003; Deleted';
+        } else props.setErr('Deletion failed');
+    }
+
     return (
         <>
             {ui === 'create' &&
                 <CreateComp setCompetition={setCompetition} setUi={setUi} setErr={props.setErr} />}
             {ui === 'edit' && <>
                 <h1>Edit competition: {competition.name}</h1>
-                <button onClick={() => setUi('round')}>&#10023; Create rounds</button>
+                <button onClick={() => setUi('create')}>&#171; Administration</button>
                 <button className='right' onClick={() => setUi('delete')}>&#10005; Delete competition</button>
                 <form onSubmit={e => e.preventDefault()}>
                     <label>Name:</label>
                     <input ref={nameRef} type='text' defaultValue={competition.name} />
                     <label>Start date:</label>
                     <input ref={dateRef} type='date' defaultValue={competition.start_date} />
-                    <label>End date:</label>
-                    <input ref={enddateRef} type='date' />
+                    <label>Venue:</label>
+                    <input ref={venueRef} type='text' defaultValue={competition.venue} />
                     <label>Info:</label>
                     <textarea ref={infoRef} defaultValue={competition.info} />
                     <button onClick={(ev) => editComp(ev)}>Save changes</button>
                 </form>
+                <h2>Rounds</h2>
+                <ul>
+                {rounds.length === 0
+                    ? <li>No rounds created</li>
+                    : rounds.map((round, idx) =>
+                        <li key={idx}>
+                            <button id={round.id} onClick={(ev) => delRound(ev)}>
+                                &#10005; Delete round
+                            </button>
+                            <p>Round id: {round.id}</p>
+                            <p>Course: {round.course}</p>
+                        </li>
+                    )
+                }
+                </ul>
+                <button onClick={() => setUi('round')}>&#10023; Create round</button>
             </>}
             {ui === 'round' &&
                 <CreateRound competition={competition} setUi={setUi} setErr={props.setErr} />}
